7278f5fa39e211b4de863c96e831cf7ed2360870
kauaDev22 <eukauapro@gmail.com>
2024-04-09 15:50:19 -0300

    Criando menssagens para BadRequestException

--- src/main/kotlin/com/mercadolivro/model/BookModel.kt
+++ src/main/kotlin/com/mercadolivro/model/BookModel.kt
@@ -1,6 +1,8 @@
 package com.mercadolivro.model
 
 import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.enums.Errors
+import com.mercadolivro.exception.BadRequestException
 import jakarta.persistence.*
 import java.math.BigDecimal
 import kotlin.jvm.Throws
@@ -30,7 +32,7 @@ data class BookModel(
     var status: BookStatus? = null
         set(value) {
             if(field == BookStatus.CANCELADO || field == BookStatus.DELETADO){
-                throw  Exception("Não é possivel alterar um livro com status ${field}")
+                throw  BadRequestException(Errors.ML102.message.format(field), Errors.ML102.code)
             }
             field = value
         }
